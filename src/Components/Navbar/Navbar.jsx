b600333534c177a0a816949d8edec59cfd896307
melad14 <miladshehata513@gmail.com>
2023-04-08 20:37:23 +0200

    edit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({userData,logOut}) {
   return<>
   <nav className="navbar navbar-expand-lg bg-transparent navbar-dark  ">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="">Navbar</Link>
+    <Link className="navbar-brand" to="">your movie</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
