c56e04bf61aabe0979d837752023c92109775a0e
melad14 <miladshehata513@gmail.com>
2023-04-09 02:11:21 +0200

    edit2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import AuthContextProvider from './Components/Context/AuthContext';
 import MediaContextProvider from './Components/Context/Store.jsx';
