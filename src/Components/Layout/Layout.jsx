bd151900152d64f72658924e0324f67189363793
melad14 <miladshehata513@gmail.com>
2023-04-29 02:06:08 +0300

    new3

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 export default function Layout({userData,logOut}) {
   return<>
   <Navbar userData={userData} logOut={logOut}/>
-  <div className="container">  <Outlet></Outlet> </div>
+  <div className="container my-2">  <Outlet></Outlet> </div>
 
   <Footer/>
   </>
